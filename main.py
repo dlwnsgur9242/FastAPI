d08ce9b974ba9c4ae99ec7e84cfb2362298e01b4
dlwnsgur9242 <dlwnsgur9242@naver.com>
2024-11-29 02:26:04 +0900

    Fix:구현 오류 해결

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_cities():
         strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
         r = requests.get(strs)
         cur_time = r.json()['datetime']
-        results.append({'name':city['name'],'timezone':city['timezone'],'current_time':cur_time})
+        results.append({'name':city['name'], 'timezone':city['timezone'], 'current_time':cur_time})
 
     return results
 
@@ -32,20 +32,16 @@ def get_city(city_id: int):
     city = db[city_id-1]
     strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
     r = requests.get(strs)
-
     cur_time = r.json()['datetime']
     return {'name':city['name'],'timezone':city['timezone'],'current_time':cur_time}
-
 
 
 @app.post('/cities')
 def create_city(city: City):
     db.append(city.dict())
-
     return db[-1]
 
 @app.delete('/cities/{city_id}')
 def delete_city(city_id: int):
     db.pop(city_id-1)
-
     return {}
